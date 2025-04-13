87a4a00461a8f75c8f82ad3334c77654cc6df76b
El Hadi Moussi <50993404+ehmoussi@users.noreply.github.com>
2025-04-13 13:48:03 +0200

    Add shadcn

--- src/App.tsx
+++ src/App.tsx
@@ -2,8 +2,9 @@
 function App() {
 
     return (
-        <>
-        </>
+        <main>
+            <div className="flex-1 content-center overflow-y-auto px-6">Test</div>
+        </main>
     )
 }
 
